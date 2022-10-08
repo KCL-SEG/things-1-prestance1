1c089998270924cab7f5d87a7e9cbdab04fa21ad
prestance1 <oudrhiri.amin@gmail.com>
2022-10-08 23:47:00 +0100

    Exercise 3

--- things/models.py
+++ things/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 
 class Thing(models.Model):
 
-    name = models.TextField()
-    description = models.TextField()
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30, blank=False, unique=True)
+    description = models.CharField(max_length=120, blank=True, unique=False)
+    quantity = models.IntegerField(unique=False, validators=[MinValueValidator(0), MaxValueValidator(100)])
